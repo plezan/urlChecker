b5c12f43e1ef2751432a48ceb1e6496109be7de9
Pierre LEZAN <pierre@lezan.pro>
2021-02-02 02:04:04 +0100

    added basic url model

--- addresses/admin.py
+++ addresses/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Address
+
 # Register your models here.
+
+class WebAddress(admin.ModelAdmin):
+    list_display = ('url','name')
+    search_fields = ['name', 'url']
+
+admin.site.register(Address, WebAddress)
--- addresses/models.py
+++ addresses/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+import re
 
 # Create your models here.
+class Address(models.Model):
+
+    url = models.TextField(
+        max_length=256,
+        verbose_name='URL',
+        null=False,
+        blank=False
+    )
+
+    name = models.TextField(
+        max_length=128,
+        verbose_name='Url name',
+        null=True,
+        blank=True,
+        default=None
+    )
+
+    def __str__(self):
+        if self.name :
+            return self.name
+        else :
+            domainName = re.sub(r"https?://",'',self.url)
+            domainName = re.match(r"^[^/\s]+",domainName).group()
+            return domainName
+
+    class Meta:
+        verbose_name = "URL"
+        verbose_name_plural = "URLs"
